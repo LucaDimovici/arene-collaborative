16ce5a016b171ad6480f4ee519b24d21f506e538
Abdul SAVADOGO <csavadogo23@gmail.com>
2026-09-25 10:10:38 +0200

    feat/conflict

--- moteur.py
+++ moteur.py
@@ -17,3 +17,5 @@ def determiner_initiative(vitesse_a: int, vitesse_b: int) -> str:
     if vitesse_a >= vitesse_b:
         return "combattant_a"
     return "combattant_b"
+
+#Moteur de Combat Occitanie
